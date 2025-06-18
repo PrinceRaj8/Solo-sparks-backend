dc54a26d7bebdd3bb94bc4e16543027638cda4eb
PrinceRaj8 <princeraj7g5@gmail.com>
2025-06-18 11:24:47 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,19 +1,86 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  mood: String,
-  personalityTraits: [String],
-  sparkPoints: { type: Number, default: 0 },
-  completedQuests: [{
-    questId: String,
-    reflectionText: String,
-    reflectionImage: String,
-    reflectionAudio: String,
-    completedAt: Date
-  }]
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minlength: 6,
+    },
+    mood: {
+      type: String,
+      default: "Neutral",
+    },
+    personalityTraits: {
+      type: [String],
+      default: [],
+    },
+    preferences: {
+      type: [String],
+      default: [],
+    },
+    sparkPoints: {
+      type: Number,
+      default: 0,
+    },
+    completedQuests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Quest",
+      },
+    ],
+    reflections: [
+      {
+        questId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Quest",
+        },
+        text: String,
+        imageUrl: String,
+        audioUrl: String,
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    rewardsUnlocked: {
+      type: [String], // store reward IDs or types
+      default: [],
+    },
+    profileBoost: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', UserSchema);
+// Encrypt password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Match password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
