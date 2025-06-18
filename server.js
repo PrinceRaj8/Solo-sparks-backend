9f1639979eb48c860ec28caef20ffecaad220f76
PrinceRaj8 <princeraj7g5@gmail.com>
2025-06-18 10:56:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,3 +15,6 @@ app.use('/api/auth', require('./routes/authRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use('/api/quests', require('./routes/questRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/rewards', require('./routes/rewardRoutes'));
