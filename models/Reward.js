f1a529aeb243b53ae31d19fff67e00fcfcab0649
PrinceRaj8 <princeraj7g5@gmail.com>
2025-06-19 12:20:19 +0530

    Update Reward.js

--- models/Reward.js
+++ models/Reward.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose');
 
 const RewardSchema = new mongoose.Schema({
-  title: String,
-  pointsRequired: Number,
-  type: { type: String, enum: ['profile_boost', 'hidden_content', 'exclusive'] },
+  title: {
+    type: String,
+    required: true,
+  },
+  pointsRequired: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  type: {
+    type: String,
+    enum: ['profile_boost', 'hidden_content', 'exclusive'],
+    required: true,
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('Reward', RewardSchema);
