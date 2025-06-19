97514034231c8cd179121973ef15a028c05c47c8
PrinceRaj8 <princeraj7g5@gmail.com>
2025-06-19 12:15:48 +0530

    Update authmiddleware.js

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -6,20 +6,20 @@ const protect = async (req, res, next) => {
 
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers.authorization.startsWith("Bearer ")
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
-
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
+      if (!req.user) {
+        return res.status(401).json({ error: "Not authorized, user not found" });
+      }
       next();
     } catch (error) {
       return res.status(401).json({ error: "Not authorized, token failed" });
     }
-  }
-
-  if (!token) {
+  } else {
     return res.status(401).json({ error: "Not authorized, no token" });
   }
 };
