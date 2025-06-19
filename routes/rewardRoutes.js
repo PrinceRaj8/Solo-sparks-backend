1007e89f00456ae745209fbd223c39f4403b9887
PrinceRaj8 <princeraj7g5@gmail.com>
2025-06-19 12:25:17 +0530

    Update rewardRoutes.js

--- routes/rewardRoutes.js
+++ routes/rewardRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getRewards, redeemReward } = require('../controllers/rewardController');
+const { protect } = require('../middleware/authMiddleware'); // Optional auth middleware
 
-router.get('/', getRewards);
-router.post('/redeem', redeemReward);
+// Public route to get all rewards (or add `protect` if needed)
+router.get('/', protect, getRewards);
+
+// Redeem reward - requires user authentication
+router.post('/redeem', protect, redeemReward);
 
 module.exports = router;
