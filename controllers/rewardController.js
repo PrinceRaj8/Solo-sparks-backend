ca6cfdde5be1f8f83099768e9da9c5a19bd4d023
PrinceRaj8 <princeraj7g5@gmail.com>
2025-06-19 12:11:11 +0530

    Update rewardController.js

--- controllers/rewardController.js
+++ controllers/rewardController.js
@@ -2,20 +2,31 @@ const Reward = require('../models/Reward');
 const User = require('../models/User');
 
 exports.getRewards = async (req, res) => {
-  const rewards = await Reward.find();
-  res.json(rewards);
+  try {
+    const rewards = await Reward.find();
+    res.json(rewards);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 };
 
 exports.redeemReward = async (req, res) => {
-  const { userId, rewardId } = req.body;
-  const user = await User.findById(userId);
-  const reward = await Reward.findById(rewardId);
+  try {
+    const { rewardId } = req.body;
+    const user = await User.findById(req.user.id);
+    const reward = await Reward.findById(rewardId);
 
-  if (user.sparkPoints >= reward.pointsRequired) {
-    user.sparkPoints -= reward.pointsRequired;
+    if (!reward) return res.status(404).json({ msg: "Reward not found" });
+    if (user.sparkPoints < reward.cost) {
+      return res.status(400).json({ msg: "Not enough Spark Points" });
+    }
+
+    user.sparkPoints -= reward.cost;
+    user.rewardsUnlocked.push(reward._id.toString());
     await user.save();
+
     res.json({ msg: "Reward redeemed!", reward });
-  } else {
-    res.status(400).json({ msg: "Not enough Spark Points" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
   }
 };
